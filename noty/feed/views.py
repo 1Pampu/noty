37a582b317f6a9e9c1ad28448a6167a5917b424e
1Pampu <piampianimartin@gmail.com>
2023-12-30 19:06:55 -0300

    unified views

--- noty/feed/views.py
+++ noty/feed/views.py
@@ -4,16 +4,17 @@ from notes.models import Note
 
 # Create your views here.
 def home(request):
-    context = {'home_active' : True}
-    return render(request, 'feed/home.html', context)
+    results = Note.objects.filter(private = 0).order_by('-date')
+    context = {'home_active' : True, 'notes' : results}
+    return render(request, 'feed/main.html', context)
 
 @login_required
 def my_notes(request):
-    results = Note.objects.filter(user = request.user)
+    results = Note.objects.filter(user = request.user).order_by('-date')
     context = {'my_notes_active' : True, 'notes' : results}
-    return render(request, 'feed/my-notes.html', context)
+    return render(request, 'feed/main.html', context)
 
 @login_required
 def following(request):
     context = {'following_active' : True}
-    return render(request, 'feed/following.html', context)
+    return render(request, 'feed/main.html', context)
